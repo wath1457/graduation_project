838cbb937703c040f56aa70c1870749785eba5ee
wath1457 <wath1457@gmail.com>
2022-09-08 02:25:57 +0900

    twitter_crawl (base)

---
+++ crawling_twit.py
@@ -0,0 +1,17 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
+# Creating list to append tweet data to
+tweets_list2 = []
+
+# Using TwitterSearchScraper to scrape data and append tweets to list
+for i,tweet in enumerate(sntwitter.TwitterSearchScraper('우울증 진단 since:2021-01-01 until:2022-09-01').get_items()):
+    if i>1000:
+        break
+    tweets_list2.append([tweet.date, tweet.content])
+    print(str(i) + "번째 트윗 수집")
+    
+# Creating a dataframe from the tweets list above
+tweets_df2 = pd.DataFrame(tweets_list2, columns=['Datetime', 'Text'])
+
+tweets_df2.to_csv('/dataset/save.csv', encoding='utf-8')
