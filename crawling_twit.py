18c2d68c1c0c55ced5045a035beb0801f8f4b6f4
wath1457 <wath1457@gmail.com>
2022-09-08 15:30:16 +0900

    Fix directory_error

--- crawling_twit.py
+++ crawling_twit.py
@@ -1,12 +1,13 @@
 import snscrape.modules.twitter as sntwitter
 import pandas as pd
+import re
 
 # Creating list to append tweet data to
 tweets_list2 = []
 
 # Using TwitterSearchScraper to scrape data and append tweets to list
 for i,tweet in enumerate(sntwitter.TwitterSearchScraper('우울증 진단 since:2021-01-01 until:2022-09-01').get_items()):
-    if i>1000:
+    if i>100:
         break
     tweets_list2.append([tweet.date, tweet.content])
     print(str(i) + "번째 트윗 수집")
@@ -14,4 +15,4 @@ for i,tweet in enumerate(sntwitter.TwitterSearchScraper('우울증 진단 since:
 # Creating a dataframe from the tweets list above
 tweets_df2 = pd.DataFrame(tweets_list2, columns=['Datetime', 'Text'])
 
-tweets_df2.to_csv('/dataset/save.csv', encoding='utf-8')
+tweets_df2.to_csv('/.dataset/save.csv', encoding='utf-8')
